9a533076a21060bb83eca0affe8e0b22ab987155
Sebastian Bormann <sebastian@bormann.net>
2021-04-29 01:12:36 +0100

    1.8.0 - Design adjustments for react-ui.

--- admin/index_m.js
+++ admin/index_m.js
@@ -2224,7 +2224,47 @@ async function load(settings, onChange) {
 						customCSS += ".table-values.highlight > tbody > tr:hover { background-color: rgba(0,0,0,0.08) !important; }";
 						customCSS += ".table-values.highlight > tbody > tr:nth-child(2n):hover { background-color: rgba(0,0,0,0.08) !important; }";
 						customCSS += ".table-values th { background-color: rgba(0,0,0,0.1) !important; color: #1d1d1d !important; }";
+
+						customCSS += ".title { background-color: #164477; }";
+
+						customCSS += ".m .tabs .tab a { color: rgba(0, 0, 0, 0.5); }";
+						customCSS += ".m .tabs .tab a:hover { color: rgba(0, 0, 0, 0.86); }";
+						customCSS += ".m .tabs .tab a.active { color: #164477; }";
+						customCSS += ".m .tabs .active { border-bottom: 2px solid #164477; }";
+
+						customCSS += ".m nav { background-color: #3399cc; }";
+
+						customCSS += ".m .btn { background-color: #e0e0e0; color: #000000; }";
+						customCSS += ".m .btn:hover { background-color: #d5d5d5; }";
+						customCSS += ".m .btn.disabled { background-color: rgba(0, 0, 0, 0.12) !important; color: rgba(0, 0, 0, 0.26) !important; }";
+
+						customCSS += ".m .btn-floating { background-color: transparent; box-shadow: none; color: #000000; }";
+						customCSS += ".m .btn-floating:hover { background-color: rgba(0,0,0,0.08); }";
+						customCSS += ".m .btn-floating.selectClear { background-color: #ffffff; }";
+						customCSS += ".m .btn-floating.selectClear:hover { background-color: #ebebeb; }";
+						customCSS += ".m .btn-floating i { color: #000000; transition: color 0.3s; }";
+						customCSS += ".m .btn-floating.red  { background-color: transparent !important; }";
+						customCSS += ".m .btn-floating.red:hover { background-color: red !important; }";
+						customCSS += ".m .btn-floating.red:hover i { color: #ffffff; }";
+												
+						customCSS += ".dialog-select-object-ids .material .main-toolbar-table .panel-button { background-color: transparent; }";
+						customCSS += ".dialog-select-object-ids .material .main-toolbar-table .panel-button:hover { background-color: rgba(0,0,0,0.08); }";
+						customCSS += ".dialog-select-object-ids .material .main-toolbar-table .panel-button i { color: #757575; }";
+						customCSS += ".dialog-select-object-ids .objects-list-table { background-color: #ffffff; }";
+						customCSS += ".dialog-select-object-ids .objects-list-table thead { display: none; }";
+						customCSS += ".dialog-select-object-ids table.objects-list-table tr { border: none !important; }";
+						customCSS += ".dialog-select-object-ids table.objects-list-table tr:hover { background-color: #3399cc; outline: none; color: #ffffff; }";
+						customCSS += ".dialog-select-object-ids table.objects-list-table tr:hover .fancytree-title { color: #ffffff; }";
+						customCSS += ".dialog-select-object-ids table.objects-list-table tr.fancytree-active { background-color: #236b8e !important; outline: none !important; color: #ffffff !important; }";
+						customCSS += ".dialog-select-object-ids table.objects-list-table tr.fancytree-active .fancytree-title { color: #ffffff; }";
+						customCSS += ".dialog-select-object-ids table.objects-list-table td { border: none !important; }";
+
+						customCSS += ".m .dropdown-content li>span { color: rgba(0, 0, 0, 0.86); }";
+						customCSS += ".m .dropdown-content li>a { color: rgba(0, 0, 0, 0.86); }";
+						customCSS += ".m [type=checkbox].filled-in:checked+span:not(.lever):after { border: #164477; background-color: #164477; }";
+						
 						addCustomCSS(customCSS, "reactCSS");
+						$('.table-button-add').addClass('grey lighten-2');
 						var selectIdImgPath = './fancytree/react/';
 						$('#fancytreeCSSLink').attr('href', './fancytree/react/ui.fancytree.min.css');
 						var warnIfLE = "5.0.8";
@@ -4792,7 +4832,8 @@ async function load(settings, onChange) {
 			}
 			//Delete file
 			if (command === 'delete_forever') {
-				$(this).find('i').addClass('red').html('delete_forever');
+				$(this).addClass('red');
+				$(this).find('i').html('delete_forever');
 				$(this).on('click', function (e) {
 					var index = $(this).data('index');
 					if(confirm(_("Delete file %s on server? Warning: This can't be undone!", images[index].filename))){
